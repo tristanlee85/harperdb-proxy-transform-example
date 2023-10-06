b490c1b2bb96fceb56f38919b1ca835ec0998d93
Kris Zyp <kriszyp@gmail.com>
2023-10-06 17:44:04 -0600

    Add routes example and lastModified

---
+++ routes/index.cjs
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = async (server, { hdbCore, logger }) => {
+  server.route({
+    url: '/getAll',
+    method: 'GET',
+    handler: async (request) => {
+      request.body= {
+        operation: 'sql',
+        sql: 'SELECT * FROM data.dogs'
+      };
+
+      return hdbCore.requestWithoutAuthentication(request);
+    }
+  });
+}
